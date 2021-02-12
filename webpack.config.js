70cd4f7c4fd1ef672ef235a28cf8974482ecba18
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-12 11:59:55 -0800

    fix for packets to allow .env support

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,65 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // new line
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  entry: {
+    main: "./src/main.js",
   },
-  devtool: 'eval-source-map',  // new line
-  devServer: {                 // new line
-    contentBase: './dist'      // new line
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    contentBase: "./dist",
   },
   plugins: [
-    new CleanWebpackPlugin(), // new line
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Template',
-      template: './src/index.html',
-      inject: 'body'
-    })
+      title: "API GROUP PROJ",
+      filename: "index.html",
+      template: "./src/index.html",
+      inject: "body",
+    }),
+    new Dotenv()
   ],
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.(gif|png|jpe?g)$/,
         use: [
-          'style-loader',
-          'css-loader'
-        ]
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets/images/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.mp3$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'assets/audio/'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "eslint-loader"
-      }
-    ]
-  }
+        loader: "eslint-loader",
+      },
+    ],
+  },
 };
